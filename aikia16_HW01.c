ef4a06cc38bafcc63d1982e76c63147486a98f03
Obie Carnathan <aikia16@uab.edu>
2023-01-20 04:43:43 +0000

    HW1 start

---
+++ aikia16_HW01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+void intro332532(int n){
+    if (n % 5 == 0 && n % 3 == 0){
+        printf("UAB CS 332&532\n");
+    }
+    if (n)
+}
+
+
+int main()
+{
+    intro332532(15);    
+}
