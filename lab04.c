210e2e8ddb33a724b275fca8af034389c22e7562
Obie Carnathan <aikia16@uab.edu>
2023-02-15 12:30:11 -0600

    Update lab04.c

--- lab04.c
+++ lab04.c
@@ -1,3 +1,9 @@
+// I, Obie Carnathan, declare that I have completed this assignment completely and entirely on my own, without any unathorized consultation from others or unathorized access to online websites. I have read the UAB Academic Honor Code and understand that any breach of the UAB Academic Honor Code may result in severe penalties.
+
+// Student Signature/Initials: OC
+
+// Date: 20230210
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
